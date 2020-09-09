e6d72638d688a3867b5e129b8a08bbc5c34a9642
echigawa0921 <yuki.echigawa@di-v.co.jp>
2020-09-09 21:55:36 +0900

    プルダウンの実装

--- db/migrate/20200909120416_create_room_users.rb
+++ db/migrate/20200909120416_create_room_users.rb
@@ -1,7 +1,8 @@
 class CreateRoomUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :room_users do |t|
-
+      t.references :room, foreign_key: true
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
