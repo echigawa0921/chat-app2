7f44e4dc50fe2a952829dbfcb48106a2cb07f27c
echigawa0921 <yuki.echigawa@di-v.co.jp>
2020-09-10 23:01:13 +0900

    投稿時刻の日本時間への修正

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,9 @@ Bundler.require(*Rails.groups)
 module ChatApp2
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 6.0 
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
 
     config.generators do |g|
       g.stylesheets false
